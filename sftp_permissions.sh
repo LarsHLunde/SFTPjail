2eccbe32f1ce7f93a84cf3cfe633645cb0afb31a
Lars Hudson Lunde <laffen47@yahoo.no>
2021-05-16 19:20:49 +0200

    Update sftp_permissions.sh

--- sftp_permissions.sh
+++ sftp_permissions.sh
@@ -7,11 +7,11 @@
 # Copyright Lars H. Lunde 2021
 
 chown root:root /sftp
-chown root:root /sftp/*
 chmod 700 /sftp
-chmod 700 /sftp/*
+chmod 750 /sftp/*
 
 for i in $(ls /sftp); do
+        chown root:$i /sftp/$i
         chown root:$i -R sftp/$i/*
         chmod g+r -R /sftp/$i/*
 done
