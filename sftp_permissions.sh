83d759880a62ed045340684d0c602a505edcf5f4
Lars Hudson Lunde <laffen47@yahoo.no>
2021-05-16 18:16:19 +0200

    Update sftp_permissions.sh

--- sftp_permissions.sh
+++ sftp_permissions.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# All of these scripts assume you run them as root
+# or by using sudo, and will have to be chmod+x'ed
+
+# MIT licensed
+# Copyright Lars H. Lunde 2021
+
 chown root:root /sftp
 chown root:root /sftp/*
 chmod 700 /sftp
